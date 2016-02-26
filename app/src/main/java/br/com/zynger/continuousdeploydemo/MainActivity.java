0f96dd576ea5dd9bb4c6a49a2220d68ec8348ddb
julioz <julioz92@gmail.com>
2016-02-25 22:07:37 -0300

    Set app message

--- app/src/main/java/br/com/zynger/continuousdeploydemo/MainActivity.java
+++ app/src/main/java/br/com/zynger/continuousdeploydemo/MainActivity.java
@@ -2,6 +2,8 @@ package br.com.zynger.continuousdeploydemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.activity_main_text);
+        textView.setText(getString(R.string.app_message, BuildConfig.VERSION_NAME));
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
