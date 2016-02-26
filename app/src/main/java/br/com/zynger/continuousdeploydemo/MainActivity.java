dd8707472971e026c77a407bcf8e5bade6404be5
julioz <julioz92@gmail.com>
2016-02-25 21:52:16 -0300

    Add project files with placeholder message

---
+++ app/src/main/java/br/com/zynger/continuousdeploydemo/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.zynger.continuousdeploydemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
